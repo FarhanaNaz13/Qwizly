78e003bdb527737deb2d18dfce51240a04bf2c8b
naz <farhana.naz@cellosco.pe>
2025-08-20 05:37:16 +0600

    modify theme cubit

--- lib/features/theme/cubit/theme_cubit.dart
+++ lib/features/theme/cubit/theme_cubit.dart
@@ -5,33 +5,6 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:quizz_app/features/core/res/string_res.dart';
 import 'package:quizz_app/features/core/utils/constant/keys.dart';
 
-// class ThemeCubit extends Cubit<ThemeMode> {
-//   static final String _key = KeyConstant.themeMode;
-//   final Box _box;
-//
-//   ThemeCubit(this._box) : super(_loadInitialTheme(_box));
-//
-//   static ThemeMode _loadInitialTheme(Box box) {
-//     final stored = box.get(_key, defaultValue: StringRes.system) as String;
-//     switch (stored) {
-//       case StringRes.dark:
-//         return ThemeMode.dark;
-//       case StringRes.light:
-//         return ThemeMode.light;
-//       default:
-//         return ThemeMode.system;
-//     }
-//   }
-//
-//   void toggleTheme() {
-//     final newMode = state == ThemeMode.dark ? ThemeMode.light : ThemeMode.dark;
-//     emit(newMode);
-//     _box.put(
-//         _key, newMode == ThemeMode.dark ? StringRes.dark : StringRes.light);
-//   }
-// }
-
-
 class ThemeCubit extends Cubit<ThemeMode> {
   static final String _key = KeyConstant.themeMode;
   final Box _box;
